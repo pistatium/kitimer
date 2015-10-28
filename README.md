bd32811a2564d6693d7bf9bf8ec5b30348121d89
Kimihiro N <kimihiro.nisimura@gmail.com>
2015-10-28 09:25:32 +0900

    Init project

---
+++ README.md
@@ -0,0 +1,18 @@
+# KiTimer
+KiTimer is timer of workers.
+
+
+## Setup
+```
+git clone  kitimer
+cd kitimer
+mkvirtualenv kitimer --python=`which python3`
+pip install -e .
+kitimer migrate
+```
+
+
+## Runserver
+```
+kitimer runserver
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kitimer',
+    version='0.0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'Django',
+    ],
+    entry_points={
+        'console_scripts': [
+            'kitimer=manage:main'
+        ]
+    }
+)
